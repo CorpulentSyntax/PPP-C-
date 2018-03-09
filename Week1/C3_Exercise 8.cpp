0061c440c429e9dac99fa25bd26106e7d4a95179
CorpulentSyntax <37209738+CorpulentSyntax@users.noreply.github.com>
2018-03-09 11:11:54 +0100

    Update C3_Exercise 8.cpp

--- Week1/C3_Exercise 8.cpp
+++ Week1/C3_Exercise 8.cpp
@@ -6,10 +6,10 @@ int main() {
 	cout << "Please input a value: ";
 	cin >> val1;
 
-	if (remainder(val1,2) == 0) {								//Als val1 / 2 = 0 dan is het getal even
+	if (remainder(val1,2) == 0) {				//Als val1 / 2 = 0 dan is het getal even
 		cout << "The value " << val1 << " is even.\n";
 	}
-	else {														//Als val1 / 2 != 0 dan is het getal oneven
+	else {							//Als val1 / 2 != 0 dan is het getal oneven
 		cout << "The value " << val1 << " is odd.\n";
 	}
 
