384b2a191802760f315dae0739a18650a8ea7d7a
CorpulentSyntax <37209738+CorpulentSyntax@users.noreply.github.com>
2018-03-09 11:10:01 +0100

    Update C4_Exercise 6.cpp

--- Week2/C4_Exercise 6.cpp
+++ Week2/C4_Exercise 6.cpp
@@ -2,17 +2,17 @@
 #include "std_lib_facilities.h"
 
 int main() {
-	vector<string> numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };										//Defines the vector for both numbers and strings
+	vector<string> numbers = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };	//Defines the vector for both numbers and strings
 	vector<string> text = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
 	string input;
 
 	cout << "Please input an int value (0-9) or the capitalized textual representation of the numbers 0-9: ";
 	while (cin >> input) {
 		bool correct = false;
-		for (int i = 0; i < text.size(); ++i) {																			//Starts the loop in which it is determinted what input is used
+		for (int i = 0; i < text.size(); ++i) {					//Starts the loop in which it is determinted what input is used
 			if (text[i] == input) {
 				cout << "The numeric representation of " << input << " is " << i << "\n";
-				correct = true;																							//Boolean used to check whether the used syntax is correct
+				correct = true;						//Boolean used to check whether the used syntax is correct
 			}
 			else if (numbers[i] == input) {
 				cout << "The textual representation of " << input << " is " << text[i] << "\n";
@@ -20,7 +20,7 @@ int main() {
 			}
 		}
 
-		if (!correct) {																									//If bool 'correct' is not true after finishing the loop this is the output
+		if (!correct) {								//If bool 'correct' is not true after finishing the loop this is the output
 			cout << "Please input according to the syntax\n";
 			system("pause");
 			exit(EXIT_FAILURE);
