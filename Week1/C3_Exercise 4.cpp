8f0d4da3a30c8e8c03b5cb35c0bb78caf737e623
CorpulentSyntax <37209738+CorpulentSyntax@users.noreply.github.com>
2018-03-09 11:10:45 +0100

    Update C3_Exercise 4.cpp

--- Week1/C3_Exercise 4.cpp
+++ Week1/C3_Exercise 4.cpp
@@ -10,21 +10,21 @@ int main()
 	
 	cout << "Sum = " << val1 + val2								
 		<< "\nProduct = " << val1 * val2 << "\n";
-	if (val1 < val2) {												//Dit wordt uitgevoerd als waarde 1 kleiner is dan waarde 2
+	if (val1 < val2) {						//Dit wordt uitgevoerd als waarde 1 kleiner is dan waarde 2
 		cout << "Smallest = " << val1 << "\n"						
 			<< "Largest = " << val2 << "\n"							
 			<< "Difference = " << val2 - val1 << "\n";				
 	}
-	else if (val1 > val2) {											//Dit wordt uitgevoerd als waarde 1 groter is dan waarde 2
+	else if (val1 > val2) {						//Dit wordt uitgevoerd als waarde 1 groter is dan waarde 2
 		cout << "Smallest = " << val2 << "\n"						
 			<< "Largest = " << val1 << "\n"						
 			<< "Difference = " << val1 - val2 << "\n";
 	}
-	else {															//Als val1 en val2 gelijk zijn dan wordt dit ook zo weergegeven
+	else {								//Als val1 en val2 gelijk zijn dan wordt dit ook zo weergegeven
 		cout << "Both values (" << val1 << " & " << val2 << ") are equal\n"
 			<< "Difference = " << val1 - val2 << "\n";
 	}
-	cout << "Ratio = " << val1 << ":" << val2 << "\n";				//Geeft de ratio tussen de 2 getallen weer
+	cout << "Ratio = " << val1 << ":" << val2 << "\n";		//Geeft de ratio tussen de 2 getallen weer
 	
 	system("pause");
 	return 0;
