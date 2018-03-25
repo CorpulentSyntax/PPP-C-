dec3ee0c8fed43e3a4ae887abd5d7dbc97e38db5
Mark <37209738+CorpulentSyntax@users.noreply.github.com>
2018-03-25 23:22:51 +0200

    Update C5_Exercise 8.cpp

--- Week3/C5_Exercise 8.cpp
+++ Week3/C5_Exercise 8.cpp
@@ -1,11 +1,13 @@
 #include "std_lib_facilities.h"
 
-int main() 
+int main()
 try {
 	int n = 0;
-	int val;
+	double val;
 	vector<int> vec;
+	vector<double> vec_exact; //This vector will be used for comparison
 	int total = 0;
+	double total_exact = 0.0;
 
 	cout << "Please enter the number of values you want to sum:\n";
 	cin >> n;
@@ -14,17 +16,17 @@ try {
 
 	while (cin >> val) {
 		vec.push_back(val); //Adds the new entered value to the front of the vector
+		vec_exact.push_back(val); ////Adds the new entered value to the front of the exact vector
 	}
 
-	if (vec.size() < n) {
-		error("You wanted to sum more values than you entered, please try again"); //You cant sum the value if it doesnt exist!
-
-	}
+	if (vec.size() < n) error("You wanted to sum more values than you entered, please try again"); //You cant sum the value if it doesnt exist!
 	else {
-		for (int y = 0; y <= n - 1; ++y) {
-			total += vec[y]; //Sums the values until the index number is n-1
+		for (int i = 0; i <= n - 1; ++i) {
+			total += vec[i]; //Sums the values until the index number is n-1
+			total_exact += vec_exact[i];
 		}
-		cout << "\nThe sum is: " << total << "\n";
+		if (total != total_exact) error("Result cannot be interpreted as an int"); //Because of rounding the two different totals can be different, if this is the case the sum cant be interpreted as an int
+		else cout << "\nThe sum is: " << total << "\n" << total_exact;
 	}
 
 	system("pause");
