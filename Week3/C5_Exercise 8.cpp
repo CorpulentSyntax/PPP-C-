bce9ba2d4edccd956ce6e9ffe1ead98097b45227
Mark <37209738+CorpulentSyntax@users.noreply.github.com>
2018-03-25 23:16:55 +0200

    Update C5_Exercise 8.cpp

--- Week3/C5_Exercise 8.cpp
+++ Week3/C5_Exercise 8.cpp
@@ -9,20 +9,20 @@ try {
 
 	cout << "Please enter the number of values you want to sum:\n";
 	cin >> n;
-	if (n <= 0) error("Value needs to be greater than zero");
+	if (n <= 0) error("Value needs to be greater than zero"); //The amount of values you want to sum can't be negative
 	cout << "\nNow enter some integers (press '/' to stop):\n";
 
 	while (cin >> val) {
-		vec.push_back(val);
+		vec.push_back(val); //Adds the new entered value to the front of the vector
 	}
 
 	if (vec.size() < n) {
-		error("You wanted to sum more values than you entered, please try again");
+		error("You wanted to sum more values than you entered, please try again"); //You cant sum the value if it doesnt exist!
 
 	}
 	else {
 		for (int y = 0; y <= n - 1; ++y) {
-			total += vec[y];
+			total += vec[y]; //Sums the values until the index number is n-1
 		}
 		cout << "\nThe sum is: " << total << "\n";
 	}
