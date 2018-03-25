e725df5af0e133745b01c2a1518aac90970e9f66
Mark <37209738+CorpulentSyntax@users.noreply.github.com>
2018-03-25 23:06:01 +0200

    Update C5_Exercise 13.cpp

--- Week3/C5_Exercise 13.cpp
+++ Week3/C5_Exercise 13.cpp
@@ -3,7 +3,7 @@
 int bulls = 0;
 int cows = 0;
 
-vector<int> itov(int n)
+vector<int> itov(int n) //Converts the int guess to an int vector
 {
 	if (n<0 || n>9999) error("Wrong input! Numbers between 0000 and 9999 only");
 	vector<int> v(4);
@@ -14,7 +14,7 @@ vector<int> itov(int n)
 	return v;
 }
 
-vector<int> new_rand_number()
+vector<int> new_rand_number() //Makes a new random number vector of numbers between 0-9
 {
 	vector<int> answer(4);
 	answer[0] = randint(9);
@@ -24,22 +24,22 @@ vector<int> new_rand_number()
 	return answer;
 }
 
-void check(vector<int> guess, vector<int> answer)
+void check(vector<int> guess, vector<int> answer) //Checks if the guess is the same as the answer
 {
 	bulls = 0;
 	cows = 0;
 	vector<char> is_bull(4, 'n');
 	vector<char> is_cow(4, 'n');
-	for (int i = 0; i<answer.size(); ++i) {
-		if (guess[i] == answer[i]) {
+	for (int i = 0; i<answer.size(); ++i) { 
+		if (guess[i] == answer[i]) { //Compares each index of the guess vector against the answer vector
 			++bulls;
-			is_bull[i] = 'y';
+			is_bull[i] = 'y'; //If the value is a bull, making the is_bull index value y "removes" this index from trying
 		}
 	}
 	for (int i = 0; i<answer.size(); ++i) {
-		if (is_bull[i] == 'n') {
+		if (is_bull[i] == 'n') { //Checks if value has been removed because of is_bull
 			for (int j = 0; j<guess.size(); ++j) {
-				if (is_bull[j] == 'n' && is_cow[j] == 'n') {
+				if (is_bull[j] == 'n' && is_cow[j] == 'n') { //Checks every other index of the answer vector against the first guess index, this is repeated for all non-removed indexes
 					if (answer[i] == guess[j]) {
 						++cows;
 						is_cow[j] = 'y';
@@ -56,20 +56,20 @@ try {
 	int seed = 0;
 	cout << "Please enter a seed: ";
 	cin >> seed;
-	srand(seed);
+	srand(seed); //the seed randomises the random numbers
 	vector<int> answer = new_rand_number();
 
 	cout << "Please enter your guess!\n";
 	while (cin >> guess) {
-		if (guess < 0 || guess > 9999) cout << "Wrong input! Numbers between 0000 and 9999 only\n";
+		if (guess < 0 || guess > 9999) cout << "Wrong input! Numbers between 0000 and 9999 only\n"; //Guess cant be negative of more than 4 digits
 		else {
 			vector<int> vec_guess = itov(guess);
 			check(vec_guess, answer);
-			if (bulls == 4) {
+			if (bulls == 4) { //The number has been guessed if the number of bulls is 4
 				cout << "You have guessed the number!\n";
-				answer = new_rand_number();
+				answer = new_rand_number(); //Sets a new answer if the guess was correct
 			}
-			if (bulls < 4) {
+			if (bulls < 4) { //Displays the amount of bulls and cows
 				cout << "Number of bulls: " << bulls << endl;
 				cout << "Number of cows: " << cows << endl;
 			}
