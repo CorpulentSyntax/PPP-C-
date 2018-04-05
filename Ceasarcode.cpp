52ea37adac381e6aacb363321b656dd18367d551
Mark <37209738+CorpulentSyntax@users.noreply.github.com>
2018-04-05 16:44:22 +0200

    Update Ceasarcode.cpp

--- Ceasarcode.cpp
+++ Ceasarcode.cpp
@@ -1,28 +1,21 @@
 #include "std_lib_facilities.h"
 
 string read_text(void) {
-	vector<string> vec_text;
 	string s;
 	cout << "Please input a sentence to encrypt. End the sentence with the letter 'q':" << endl;
 
-	for (string text; cin >> text;) {
-		vec_text.push_back(text);
-		if (text == "q") {
-			for (int i = 0; i < vec_text.size() - 1; ++i) {
-				s += vec_text[i];
-			}
-			break;
-		}
+	for (string input; cin >> input;) {
+		if (input == "q") break; //If the input is 'q' the loop stops
+		s += input; //Removes whitespaces and concatenates string
 	}
-
 	return s;
 }
 
 string string_to_upper(string s) {
 	for (int i = 0; i < s.length(); ++i) {
-		if (s[i] > 96 && s[i] < 123) s[i] -= 32;
-		if (s[i] > 64 && s[i] < 91) ;
-		else s[i] = 69;
+		if (s[i] > 96 && s[i] < 123) s[i] -= 32; //If the char is lowercase change it to uppercase
+		if (s[i] > 64 && s[i] < 91) ; //Do nothing if char is already uppercase
+		else s[i] = 69; //All other chars become 'E'
 	}
 
 	return s;
@@ -33,10 +26,10 @@ string ceasar_code(string s, int offset) {
 	cin >> offset;
 
 	if (offset < 0) error("Offset can't be negative!");
-	if (offset > 26) error("Offset too large!");
+	if (offset > 26) error("Offset too large!"); //Offset > 26 doesnt make sense, alphabet only has 26 letter and thus only 26 offsets make sense
 	for (int i = 0; i < s.length(); ++i) {
-		s[i] += offset;
-		if (s[i] > 90) s[i] -= 26;
+		s[i] += offset; //Offsets each letter by the desired amount
+		if (s[i] > 90) s[i] -= 26; //If the offset passes the char 'Z' it continues at char 'A'
 	}
 	
 	return s;
@@ -49,17 +42,17 @@ void output_text(string s, int block_size, char delimeter) {
 	
 	vector<string> split_in_block;
 	for (int i = 0; i < s.length();  i += block_size) {
-		split_in_block.push_back(s.substr(i, block_size));
+		split_in_block.push_back(s.substr(i, block_size)); //Makes a vector containing 1 block per index
 	}
 	
 	cout << endl << "Your super secret spy code is: " << endl;
 
 	int total_del = split_in_block.size() - 1;
-	for (int i = 0; i < split_in_block.size(); ++i) {
-		cout << split_in_block[i];
+	for (int i = 0; i < split_in_block.size(); ++i) { //Posts a block followed by a delimeter
+		cout << split_in_block[i]; 
 		if (total_del > 0) {
 			cout << delimeter;
-			total_del -= 1;
+			total_del -= 1; //Makes sure the last block isn't followed by a delimeter
 		}
 	}
 
