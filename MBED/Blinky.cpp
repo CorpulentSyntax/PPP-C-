486cc0cbe02e888619276d43df65d131a3566873
Mark <37209738+CorpulentSyntax@users.noreply.github.com>
2018-05-27 17:58:50 +0200

    Update Blinky.cpp

--- MBED/Blinky.cpp
+++ MBED/Blinky.cpp
@@ -3,18 +3,20 @@
 DigitalIn mybutton(USER_BUTTON);
 DigitalOut myled(A0);
  
+void morse_sos(){
+   for (int i = 0; i <3; ++i) {
+     for (int j = 0; j < 3; ++j) {
+        myled = !myled; // Toggle the LED state
+        if (i % 2 == 0) wait(0.5); // The first and third time this loop is executed the led will remain on for 500 ms each time (short blink)
+        if (i % 2 == 1) wait(1); // The second time this loop is executed the led will remain on for 1 s each time (long blink)
+        myled = !myled;
+        wait(0.5);
+     } 
+   }
+ } 
+ 
 int main() {
   while(1) {
-    if (mybutton == 0) { // Button is pressed
-        for (int i = 0; i <3; ++i) {   
-            for (int j = 0; j < 3; ++j) {
-               myled = !myled; // Toggle the LED state
-               if (i % 2 == 0) wait(0.5);
-               if (i % 2 == 1) wait(1); // 500 ms
-               myled = !myled;
-               wait(0.5);
-              }
-        }
-     }
+    if (mybutton == 0) morse_sos(); // Button is pressed
    }
  }
